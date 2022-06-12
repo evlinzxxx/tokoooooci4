7be283146bcc5373cbb5c5dc1c13cacbe38c3443
Evlin Sitanggang <105533381+evlinzxxx@users.noreply.github.com>
2022-06-12 12:34:45 +0700

    Update C_Auth.php

--- app/Controllers/C_Auth.php
+++ app/Controllers/C_Auth.php
@@ -38,44 +38,52 @@ class C_Auth extends BaseController
                 $user->password = $this->request->getPost('password');//meng-assign field password, dengan value post sebelumnnya
                 //password langsung terhubung ke entities User
 
-                $user->created_by = 0;
-                $user->created_date = date("Y-m-d H:i:s");
+                $user->created_by = 0;//yang membuat akun
+                $user->created_date = date("Y-m-d H:i:s");//tanggal akun dibuat
 
-                $modelUser->save($user);
+                $modelUser->save($user);//simpan data user ke dalam model
 
-                return view('login');
+                return view('login');//arahkan ke view login
             }
-
+            //jika salah tampilkan pesan error
             $this->session->setFlashdata('errors', $errors);
         }
-
+        //kembalikan ke form register
         return view('register');
     }
 
     //function untuk login
     public function login()
     {
+        //mengambil data login yang diinputkan user
+        //jalankan validasi data yanng dipost
         if ($this->request->getPost()) {
             $data = $this->request->getPost();
             $validate = $this->validation->run($data, 'login');
             $errors = $this->validation->getErrors();
 
+            //jika ada error return view login kembali
             if ($errors) {
                 return view('login');
             }
 
             $modelUser = new \App\Models\M_User();
 
-            $username = $this->request->getPost('username');
-            $password = $this->request->getPost('password');
-
+            $username = $this->request->getPost('username');//menyimpan data username di var username
+            $password = $this->request->getPost('password');//menyimpan data password di var password
+            //cek username yang dipost apakah sama dengan username di db,simpan dalam var user
             $user = $modelUser->where('username', $username)->first();
 
+            //jika user ditemukan
             if ($user) {
-                $password2 = $user->password2;
-                if ($user->password !== md5($password2 . $password)) {
-                    $this->session->setFlashdata('errors', ['Password Salah!']);
+                $password2 = $user->password2;//mengambil password2 dari user yg ditemukan
+                //jika password yg dipost tidak sama dengan password dan password2 di db
+                if ($user->password !== md5($password2 . $password))
+                {
+                    $this->session->setFlashdata('errors', ['Password Salah!']);//tampil pesan error
+                //jika password yg dipost sama dengan password dan password2 di db
                 } else {
+                    //session data dalam array
                     $sessData = [
                         'username' => $user->username,
                         'id' => $user->id,
@@ -83,11 +91,13 @@ class C_Auth extends BaseController
                         'isLoggedIn' => TRUE
                     ];
 
-                    $this->session->set($sessData);
+                    $this->session->set($sessData);//menyimpan session
 
+                    //arahkan ke controller C_Etalase di funtion index
                     return redirect()->to(site_url('C_Etalase/index'));
                 }
             } else {
+                //pesan error jika user tidak ditemukan
                 $this->session->setFlashdata('errors', ['User Tidak Ditemukan :( ']);
             }
         }
@@ -97,7 +107,8 @@ class C_Auth extends BaseController
     //function untuk logout
     public function logout()
     {
+        //session untuk logout
         $this->session->destroy();
-        return redirect()->to(site_url('C_Auth/login'));
+        return redirect()->to(site_url('C_Auth/login'));//diminta untuk kembali login
     }
 }
