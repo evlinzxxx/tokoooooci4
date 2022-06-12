f62ff3d62305f8e586f355308de896b755ef1760
Evlin Sitanggang <105533381+evlinzxxx@users.noreply.github.com>
2022-06-12 14:08:44 +0700

    Update C_Transaksi.php

--- app/Controllers/C_Transaksi.php
+++ app/Controllers/C_Transaksi.php
@@ -16,9 +16,11 @@ class C_Transaksi extends BaseController
     //function untuk menampilkan detail invoice transaksi ke admin
     public function view()
     {
+        //ambil id dri segment ke 3 etalase beli 
         $id = $this->request->uri->getSegment(3);
 
         $transaksiModel = new \App\Models\M_Transaksi();
+        //join 3 tabel, tabel user, tabel barang, tabel transaksi
         $transaksi = $transaksiModel->select('*, tb_transaksi.id AS id_trans')->join('tb_barang', 'tb_barang.id=tb_transaksi.id_barang')
             ->join('tb_user', 'tb_user.id=tb_transaksi.id_pembeli')
             ->where('tb_transaksi.id', $id)
@@ -32,9 +34,11 @@ class C_Transaksi extends BaseController
     //function untuk menampilkan detail invoice transaksi ke customer
     public function view_cust()
     {
+         //ambil id dri segment etalase beli 
         $id = $this->request->uri->getSegment(3);
 
         $transaksiModel = new \App\Models\M_Transaksi();
+        //join 3 tabel, tabel user, tabel barang, tabel transaksi
         $transaksi = $transaksiModel->select('*, tb_transaksi.id AS id_trans')->join('tb_barang', 'tb_barang.id=tb_transaksi.id_barang')
             ->join('tb_user', 'tb_user.id=tb_transaksi.id_pembeli')
             ->where('tb_transaksi.id', $id)
@@ -49,6 +53,7 @@ class C_Transaksi extends BaseController
     public function index()
     {
         $transaksiModel = new \App\Models\M_Transaksi();
+        //menyimpan data transaksi dan pager dalam array dengan var data
         $data = [
             $model = $transaksiModel->findAll(),
             'transaksi' => $transaksiModel->paginate(5),
@@ -62,23 +67,27 @@ class C_Transaksi extends BaseController
     //function untuk mencetak detail invoice 
     public function invoice()
     {
+         //ambil id dri segment ke 3 etalase beli 
         $id = $this->request->uri->getSegment(3);
 
         $modelTransaksi = new \App\Models\M_Transaksi();
-        $transaksi = $modelTransaksi->find($id);
+        $transaksi = $modelTransaksi->find($id);//temukan data pada model transaksi melalui id
 
         $modelUser = new \App\Models\M_User();
-        $pembeli = $modelUser->find($transaksi->id_pembeli);
+        $pembeli = $modelUser->find($transaksi->id_pembeli);//temukan id pembeli di dalam data transaksi yg sudah ditemukan di dalam model user
+
 
         $modelBarang = new \App\Models\M_Barang();
-        $barang = $modelBarang->find($transaksi->id_barang);
+        $barang = $modelBarang->find($transaksi->id_barang);//temukan id barang di dalam data transaksi yg sudah ditemukan di dalam model barang
 
+        //masukkan data yang sudah ditemukan ke dalam var html
         $html = view('transaksi/invoice', [
             'transaksi' => $transaksi,
             'pembeli' => $pembeli,
             'barang' => $barang
         ]);
 
+        //cetak pdf
         $pdf = new TCPDF('L', PDF_UNIT, 'A4', true, 'UTF-8', false);
 
         $pdf->SetCreator(PDF_CREATOR);
